850c8b3e7e4c77458fb04063e2678cc4423f4c9a
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-09 15:21:37 +0000

    add dislikes to the list.

--- js/main.js
+++ js/main.js
@@ -29,6 +29,16 @@ function renderHTML(data) {
             htmlString += " and " + data[i].foods.likes[ii];
         }
     }
+
+    htmlString += ' and dislikes ';
+
+            for(ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+            if (ii == 0) {
+            htmlString += data[i].foods.dislikes[ii];    
+            } else {
+            htmlString += " and " + data[i].foods.dislikes[ii];
+        }
+    }
         htmlString += '.</p>';
     }
     animalContainer.insertAdjacentHTML('beforeend', htmlString);
