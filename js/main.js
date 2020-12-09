61daeab404c531b899a2bf930b8f2553a1efc922
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-09 15:31:00 +0000

    Include error message.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,19 @@ btn.addEventListener("click", function() {
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
 ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    } else {
+        console.log("We connected to the server, but it returned an error.");
+    }
+    
 };
+
+ourRequest.onerror = function() {
+    console.log("Connection erroor");
+};
+
 ourRequest.send();
 pageCounter++;
 if (pageCounter > 3) {
